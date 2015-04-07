8649170baaa04a4bd21123d528c2f69dfccca0c0
oxoxll <oxoxll@qq.com>
2015-04-07 17:49:15 +0800

    chat

--- www/js/app.js
+++ www/js/app.js
@@ -17,10 +17,7 @@ angular.module('drunken', ['ionic', 'drunken.controllers', 'drunken.services', '
 
 .config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
+
   $stateProvider
 
   // setup an abstract state for the tabs directive
@@ -102,7 +99,6 @@ angular.module('drunken', ['ionic', 'drunken.controllers', 'drunken.services', '
 
   ;
 
-  // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/bbss');
   $ionicConfigProvider.tabs.position('bottom');
   $ionicConfigProvider.tabs.style('standard');
